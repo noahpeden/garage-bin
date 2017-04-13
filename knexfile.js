e55b117c860ae819acf2642493400bd686468f42
Noah Peden <noahpeden@gmail.com>
2017-04-13 12:36:20 -0600

    added Procfile

--- knexfile.js
+++ knexfile.js
@@ -24,14 +24,14 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds/production'
-    },
-    useNullAsDefault: true
-  }
+     client: 'pg',
+     connection: process.env.HEROKU_POSTGRESQL_SILVER_URL + `?ssl=true`,
+     migrations: {
+       directory: './db/migrations'
+     },
+     seeds: {
+       directory: './db/seeds/production'
+     },
+     useNullAsDefault: true
+   }
 };
